6ed4fb058d036df7f5f00c6f2db9e8177af7ad46
PeZunino <pedrozsantos@hotmail.com>
2025-01-11 11:23:07 -0300

    knex

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const server = fastify();
 
 
 server.get("/hello", () => {
-  return "Hello World";
+  return "Hello World"
 });
 
 server.listen({ port: 3333 }).then(() => console.log("Server listing on 3333"));
