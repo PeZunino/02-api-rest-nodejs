30ba2d1a683c250230807c73fa4949532fce4c7c
PeZunino <pedrozsantos@hotmail.com>
2025-01-11 18:33:07 -0300

    eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,34 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  
 	{
 		files: ['**/*.{js,mjs,cjs,ts}'],
 		languageOptions: { globals: globals.node },
-		plugins:{
-			'@stylistic': stylistic    
+		plugins: {
+			'@stylistic': stylistic,
+			'simple-import-sort': simpleImportSort,
 		},
-		rules:{
+		rules: {
 			semi: 'error',
-			'@stylistic/newline-per-chained-call':['error',{ 'ignoreChainWithDepth': 1 }],
+			'@stylistic/newline-per-chained-call': ['error', { 'ignoreChainWithDepth': 1 }],
 			'@stylistic/indent': ['error', 'tab'],
-			'@stylistic/quotes': ['error', 'single']
-		}
+			'@stylistic/quotes': ['error', 'single'],
+			'@stylistic/object-property-newline': 'error',
+			'@stylistic/no-multi-spaces': 'error',
 
+			'simple-import-sort/imports': [
+				'error',
+				{
+					groups: [['^\\u0000', '^node:', '^@?\\w', '^', '^\\.']]
+				},
+			],
+		},
 	},
-
 	pluginJs.configs.recommended,
-
+	...tseslint.configs.recommended,
 ];
